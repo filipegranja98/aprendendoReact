c7878339f773516e1996e5305bad693241dd281c
Filipe da Silva Granja <110956203+filipegranja98@users.noreply.github.com>
2023-11-21 00:04:12 -0300

    firts commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import Header from './Header'
+
+import { ChakraProvider, CSSReset, extendTheme } from '@chakra-ui/react';
+import './fonts.css'
+import { Outlet } from 'react-router-dom'
+
+const theme = extendTheme({
+  
+});
+
+function App() {
+  return (
+    <ChakraProvider theme={theme}>
+      <CSSReset />
+    <div>
+    <Header />
+    <Outlet/>
+    </div>
+    </ChakraProvider>
+  )
+}
+export default App;
